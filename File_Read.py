7b7ac685371934fd36353fbb832c717b147c9a7f
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-21 20:20:47 +0530

    Add files via upload

---
+++ File_Read.py
@@ -0,0 +1,17 @@
+#open function is open also as well create also
+def main():
+    try:
+        fobj = open("Demo.txt","r")
+        print("file gets opened")
+     
+
+        Data = fobj.read(10)
+        print(Data)
+        fobj.close()
+    
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+
+
+if __name__ == "__main__":
+    main()
