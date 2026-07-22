3631eb1143925d3b35374635f71cd4c2dcb2751d
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-07-22 22:31:58 +0530

    Add FileOpenException.py to handle file opening errors

---
+++ FileOpenException.py
@@ -0,0 +1,12 @@
+
+def main():
+    try:
+        open("Demo.txt","r")
+        print("file gets opened")
+    
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+
+
+if __name__ == "__main__":
+    main()
